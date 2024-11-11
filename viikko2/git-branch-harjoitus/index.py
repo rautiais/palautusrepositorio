5b2b43c27c5226e046ae67849dd13d5d174236f4
rautiais <rautiainen.e.salla@gmail.com>
2024-11-11 13:22:37 +0200

    Index korjaus

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -4,7 +4,7 @@ from logger import logger
 from summa import summa
 from erotus import erotus
 
-logger("aloitetaan")
+logger("aloitetaan ohjelma") # muutos mainissa
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
@@ -12,3 +12,4 @@ print(f"{summa(x, y)}")
 print(f"{erotus(x, y)}")
 
 logger("lopetetaan")
+print("goodbye!") # lisäys bugikorjaus-branchissa
